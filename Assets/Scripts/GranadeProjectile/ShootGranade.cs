a0a4bb2f422ed8ccbd9913ec4a155ddc8fe16f16
Felix Rönnholm <felix.ronnholm@gmail.com>
2020-11-15 07:49:36 +0800

    barrels and boxes

---
+++ Assets/Scripts/GranadeProjectile/ShootGranade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootGranade : MonoBehaviour
+{
+    public Granade GranadePrefab;
+    public void ShootGranadeAtPlayer(Vector3 targetPos)
+    {
+        Instantiate(GranadePrefab, transform.position, transform.rotation);
+        //Rigidbody granada = Grana
+    }
+
+}
+//

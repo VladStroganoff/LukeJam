c408c3683b366fcd4f76aabc27976c934f74c479
Hugzey <Lukehuggett@live.com.au>
2020-11-14 21:42:47 +0800

    Split out example target behaviour

--- Assets/Scripts/ITarget.cs
+++ Assets/Scripts/ITarget.cs
@@ -1,17 +1,7 @@
-﻿using UnityEngine;
-
-namespace Assets.Scripts
+﻿namespace Assets.Scripts
 {
     public interface ITarget
     {
         public void Hit();
-    }
-
-    public class ExampleTargetBehaviour : MonoBehaviour, ITarget
-    {
-        public void Hit()
-        {
-            Debug.Log("Ouch! I cannot beleive you shot me!");
-        }
     }
 }

f26d0f99ea88a61279d33379163f1fb1c86d3923
Hugzey <Lukehuggett@live.com.au>
2020-11-14 21:09:13 +0800

    Split Target Example to new .cs

---
+++ Assets/Scripts/ExampleTargetBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ExampleTargetBehaviour : MonoBehaviour, ITarget
+    {
+        public void Hit()
+        {
+            Debug.Log("Ouch! I cannot beleive you shot me!");
+        }
+    }
+}

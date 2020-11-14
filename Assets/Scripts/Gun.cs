65afcf2ae0ce118a047a270d3ade2043d1622e4c
Hugzey <Lukehuggett@live.com.au>
2020-11-14 21:04:23 +0800

    Created Gun fire raycast Created ITarget Interface Created ITarget example implementation
    All raycast hit targets with ITarget implementing classes then have their Hit() methods invoked.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Gun : MonoBehaviour
+namespace Assets.Scripts
 {
-    public List<ParticleSystem> Particles = new List<ParticleSystem>();
-    public List<AudioSource> Audio = new List<AudioSource>();
-
-    void Start()
+    public class Gun : MonoBehaviour
     {
-        
-    }
+        public List<ParticleSystem> Particles = new List<ParticleSystem>();
+        public List<AudioSource> Audio = new List<AudioSource>();
+        public float RayDistance = 1000f;
+        private List<Transform> _bones = new List<Transform>();
+        private Transform _muzzleBone;
 
-    public void ShootingGraphics()
-    {
-        foreach(ParticleSystem sys in Particles)
+        void Start()
         {
-            sys.Emit(1);
+
+            foreach (var bone in GameObject.FindGameObjectsWithTag("Bone"))
+            {
+                _bones.Add(bone.transform);
+                Debug.Log(bone.name);
+                if (bone.name == "Bone_Muzzle")
+                    _muzzleBone = bone.transform;
+            }
+        }
+
+        public void Fire()
+        {
+            var ray = new Ray(_muzzleBone.position, _muzzleBone.forward);
+            var cast = Physics.Raycast(ray, out var rayHit, RayDistance, 0, QueryTriggerInteraction.UseGlobal);
+            foreach (ITarget target in rayHit.transform.gameObject.GetComponents<ITarget>())
+            {
+                target.Hit();
+            }
+        }
+
+        public void ShootingGraphics()
+        {
+            foreach (ParticleSystem sys in Particles)
+            {
+                sys.Emit(1);
+            }
         }
     }
 }

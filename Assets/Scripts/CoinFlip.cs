0d133315d1eac4619868d966152789462b03e476
Hugzey <Lukehuggett@live.com.au>
2020-11-15 16:18:31 +0800

    Created whole minigame, Coin Juggle

---
+++ Assets/Scripts/CoinFlip.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CoinFlip : MonoBehaviour, ITarget
+    {
+        public float GroundHeight = 0f;
+        private bool _started = false;
+        private bool _gameOver = false;
+        private Rigidbody _rigidBody;
+        public int FlipCounter = 0;
+        public int WinScore = 3;
+        public float ForceMultiplier = 2f;
+        public TextMesh ScoreCounter;
+
+        void Start()
+        {
+            _rigidBody = gameObject.GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (transform.position.y < GroundHeight)
+                GameOver();
+        }
+
+        private void GameOver()
+        {
+            _gameOver = true;
+            _rigidBody.isKinematic = true;
+            transform.position.Set(transform.position.x, GroundHeight + 0.1f, transform.position.z);
+            if (FlipCounter < WinScore)
+                Lose();
+            Win();
+        }
+
+        private void Win()
+        {
+
+        }
+
+        private void Lose()
+        {
+
+        }
+
+        public void Hit()
+        {
+            if (!_started)
+                StartFlipping();
+            if(!_gameOver)
+                Flip();
+        }
+
+        private void StartFlipping()
+        {
+            _started = true;
+            _rigidBody.useGravity = true;
+        }
+
+        private void Flip()
+        {
+            FlipCounter++;
+            ScoreCounter.text = FlipCounter.ToString();
+            _rigidBody.AddForce(Random.Range(-0.05f, 0.05f), 1f * ForceMultiplier, Random.Range(-0.05f, 0.05f), ForceMode.Impulse);
+        }
+    }
+}

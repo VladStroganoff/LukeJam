f0d01f170cb55d09e46aeabe414d101ad0900b66
Felix Rönnholm <felix.ronnholm@gmail.com>
2020-11-15 10:09:06 +0800

    GUN!

---
+++ Assets/Scripts/GranadeProjectile/ShootGranade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootGranade : MonoBehaviour
+{
+    public Granade GranadePrefab;
+    public void ShootGranadeAtPlayer(Vector3 targetPos)
+    {
+        Instantiate(GranadePrefab, transform.position, transform.rotation);
+    }
+
+}
